3f0865d542908b3b11ad81c1ffdb63c5113f9cdf
coderman0 <81552900+coderman0@users.noreply.github.com>
2026-06-10 20:13:13 +0300

    массивы

---
+++ massive.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Program
+{
+  static void Main()
+  {
+    int[] array = {1 ,2 ,3 ,4 ,5};
+
+    
+
+    Console.WriteLine(array[0]);
+
+
+
+    // Declare a single-dimensional array of 5 integers.
+int[] array1 = new int[5] {51, 52, 53, 54, 55 };
+    {
+      foreach (int number in array1)
+      {
+        Console.WriteLine(number);
+      }
+    }
+
+
+  }
+}
