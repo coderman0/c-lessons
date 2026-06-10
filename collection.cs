7c30b6891f3a0ac14af679e8e95d814cec17272a
coderman0 <81552900+coderman0@users.noreply.github.com>
2026-06-10 20:24:35 +0300

    последний урок коллекции

---
+++ collection.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+  static void Main()
+{
+  List<string> names = new List<string> {"Aneliya ", "Emir "};
+
+  names.Add("Ridvan");
+
+  Console.WriteLine(" Первый элемент: " + names[2]);
+
+}
+}
